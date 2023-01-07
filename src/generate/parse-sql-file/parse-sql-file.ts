3c7dc73dab3c5cc858318933a2ed92dbd026ede1
matt <matt@brainmedialearning.com>
2023-01-07 18:14:08 -0500

    normalize file paths to unix style

--- src/generate/parse-sql-file/parse-sql-file.ts
+++ src/generate/parse-sql-file/parse-sql-file.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from 'fs';
-import { basename } from 'path';
+import { basename, sep, posix } from 'path';
 import { ParsedSQLFile } from '../../interfaces/parsed-sql-file';
 import { Lexer } from './lexer';
 import { Parser } from './parser';
 
-export function parseSQLFile(fullFilePath: string, fileIdentifier: string): ParsedSQLFile {
-    const contents = readFileSync(fullFilePath).toString();
+export function parseSQLFile(filePath: string, fileIdentifier: string): ParsedSQLFile {
+    const contents = readFileSync(filePath).toString();
+    const fullFilePath = filePath.split(sep).join(posix.sep);
     const fileName = basename(fullFilePath);    
     const fileTokens = new Lexer(contents).lex();
     const sqlStatements = new Parser(fileTokens, fileName, fullFilePath, fileIdentifier).parse();
